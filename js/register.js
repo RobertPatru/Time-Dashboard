7ef7fba4891337539ab4be367e5f08aa8d904f09
RobertPatru <86546157+RobertPatru@users.noreply.github.com>
2021-12-01 16:01:07 +0200

    Add files via upload

---
+++ js/register.js
@@ -0,0 +1,77 @@
+// Register Page
+class Account {
+    constructor (email, password, confirmPassword) {
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    confirmPasswordFunction() {
+        if (this.password != this.confirmPassword || this.password == '' || this.confirmPassword == '' || this.email == '' || this.email.includes('@') != true || (this.email.length > 7) == false || (this.password.length > 7) == false) {
+            document.querySelector('.sub-div-password').style.borderColor = 'red';
+            document.querySelector('.sub-div-password .fa-lock').style.color = 'red';
+            document.querySelector('.sub-div-confirm-password').style.borderColor = 'red';
+            document.querySelector('.sub-div-confirm-password .fa-lock').style.color = 'red';         
+            document.querySelector('.sub-div-email').style.borderColor = 'red';
+            document.querySelector('.sub-div-email .fa-user').style.color = 'red';      
+            
+            // make the inputs black again, after 3 sec
+            setTimeout(function() {
+                document.querySelector('.sub-div-password').style.borderColor = 'black';
+            document.querySelector('.sub-div-password .fa-lock').style.color = 'black';
+            document.querySelector('.sub-div-confirm-password').style.borderColor = 'black';
+            document.querySelector('.sub-div-confirm-password .fa-lock').style.color = 'black';         
+            document.querySelector('.sub-div-email').style.borderColor = 'black';
+            document.querySelector('.sub-div-email .fa-user').style.color = 'black'; 
+            }, 3000);
+        }
+        else if (this.password == this.confirmPassword && this.password != '' && this.email.includes('@') == true) {
+            document.querySelector('.container-sing-in-page').style.display = 'none';
+            document.querySelector('.welcome-message-register').textContent = 'Account Successfully Created';
+
+            setTimeout(function(){
+                window.location.href = '/index.html';
+            }, 3000);
+
+            this.addAccountInLocalStorage();
+        }
+    }
+
+    addAccountInLocalStorage() {
+        let emails;
+
+        if (localStorage.getItem('email') === null) {
+            emails = [];
+            console.log(`empty local storage`, localStorage);
+        } else {
+            emails = JSON.parse(localStorage.getItem('email'));
+        }
+
+        emails.push(this.email);
+        localStorage.setItem('email', JSON.stringify(emails));
+        
+
+        let passwords;
+
+        if (localStorage.getItem('password') === null) {
+            passwords = [];
+        } else {
+            passwords = JSON.parse(localStorage.getItem('password'));
+        }
+
+        passwords.push(this.password);
+        localStorage.setItem('password', JSON.stringify(passwords));
+    }
+}
+
+document.getElementById('register-account').addEventListener('click', function(object) {
+    const email = document.getElementById('register-email').value;
+    const password = document.getElementById('register-password').value;
+    const confirmPassword = document.getElementById('register-password-confirm').value;
+
+    const marce = new Account(email, password, confirmPassword);
+    
+    marce.confirmPasswordFunction();
+
+    object.preventDefault();
+});
